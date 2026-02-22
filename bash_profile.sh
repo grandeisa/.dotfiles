9a22e681c35179a31764b0973cdde6dc69903ef2
IsaelGabriel <isaelgsfaria@gmail.com>
2026-02-22 17:50:33 -0300

    Add default waybar config

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,2 +1,4 @@
 export DOTFILES="$HOME/.dotfiles"
 export NIRI_CONFIG="$DOTFILES/niri/config.kdl"
+export WAYBAR_STYLE="$DOTFILES/waybar/style.css"
+export WAYBAR_CONFIG="$DOTFILES/waybar/config.jsonc"
