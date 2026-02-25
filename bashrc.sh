5d1cd8f8a495f6bbfb3bad6e95a8e3663297a42c
IsaelGabriel <isaelgsfaria@gmail.com>
2026-02-25 02:09:47 -0300

    Add color to name in bash prompt

--- bashrc.sh
+++ bashrc.sh
@@ -1,2 +1,2 @@
 source $DOTFILES/git-prompt.sh
-PROMPT_COMMAND='PS1_CMD1=$(__git_ps1 "(%s)")'; PS1='\n\u@\h \[\e[2m\]\W\[\e[0m\] \[\e[96;1m\]${PS1_CMD1}\[\e[0m\] λ '
+PROMPT_COMMAND='PS1_CMD1=$(__git_ps1 "(%s)")'; PS1='\n\[\e[33m\]\u\[\e[m\]@\h \[\e[2m\]\W\[\e[0m\] \[\e[96;1m\]${PS1_CMD1}\[\e[0m\] λ '
